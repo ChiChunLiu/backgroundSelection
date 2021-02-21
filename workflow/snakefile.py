5905a6adacee8be5b413c61eb386930f3c547e7a
Chi-Chun Liu <chichun.liu81@gmail.com>
2021-02-21 02:00:17 -0600

    update prefersim rules

--- workflow/snakefile.py
+++ workflow/snakefile.py
@@ -3,27 +3,27 @@ configfile: 'config/config.yaml'
 
     
 N = 5000
-N_bottleneck = 500
+N_bottleneck = config['params']['N_bottleneck']
 
 Bs = [0.4, 0.6, 0.8] # strengths of BGS
 ss = [2e-4, 2e-3, 2e-2, 2e-1] # selection coeffs of focal sites
 Ns = [2000, 3000, 4000, 5000] # population sizes
-reps = np.arange(1, 101, dtype=int) # n. of replicates
-cache_spectrum_nss = np.arange(10, 15, 5, dtype=int) # sfs sample sizes
+reps = np.arange(1, 2, dtype=int) # n. of replicates
+cache_spectrum_nss = np.arange(10, 15, 25, dtype=int) # sfs sample sizes
 demo_models = ['constant', 'bottleneck']
 rs = [0, 0.1, 0.2] # recombination rates
 
 rule run_all:
     input:
-        expand('{}/{}'.format(config['paths']['data'], 'slim/bgs_1class/bgs_1class.B{B}_N5000_rep{rep}.trees'), rep = reps, B = Bs),
-        expand('{}/{}'.format(config['paths']['data'], 'slim/bgs_1class/bgs_focal.rep{rep}.trees'), rep = reps),
-        expand('{}/{}'.format(config['paths']['data'], 'slim/bgs_3classes/bgs_3classes.B{B}_N{N}_rep{rep}.trees'), B = Bs, rep = reps, N = Ns),
-        expand('{}/{}'.format(config['paths']['data'], 'slim/bgs_3classes_noneq/bgs_bottleneck_noMutation.N500_rep{rep}.trees'), rep = reps),
-        expand('{}/{}'.format(config['paths']['data'], 'slim/bgs_3classes_noneq/bgs_bottleneck_3classes.N500_rep{rep}.trees'), rep = reps),
-        expand('{}/{}'.format(config['paths']['output'], '{cache_demo}_spectra_n{ns}.bpkl'), ns = cache_spectrum_nss, cache_demo = demo_models),
-        expand('{}/{}'.format(config['paths']['params'], 'PReFerSim/{demo_model}/param.txt'), demo_model = demo_models),
-        expand('{}/{}'.format(config['paths']['params'], 'PReFerSim/{demo_model}/demog.txt'), demo_model = demo_models)
-
+#         expand('{}/{}'.format(config['paths']['data'], 'slim/bgs_1class/bgs_1class.B{B}_N5000_rep{rep}.trees'), rep = reps, B = Bs),
+#         expand('{}/{}'.format(config['paths']['data'], 'slim/bgs_1class/bgs_focal.rep{rep}.trees'), rep = reps),
+#         expand('{}/{}'.format(config['paths']['data'], 'slim/bgs_3classes/bgs_3classes.B{B}_N{N}_rep{rep}.trees'), B = Bs, rep = reps, N = Ns),
+#         expand('{}/{}'.format(config['paths']['data'], 'slim/bgs_3classes_noneq/bgs_bottleneck_noMutation.N500_rep{rep}.trees'), rep = reps),
+#         expand('{}/{}'.format(config['paths']['data'], 'slim/bgs_3classes_noneq/bgs_bottleneck_3classes.N500_rep{rep}.trees'), rep = reps),
+#         expand('{}/{}'.format(config['paths']['output'], '{cache_demo}_spectra_n{ns}.bpkl'), ns = cache_spectrum_nss, cache_demo = demo_models),
+#         expand('{}/{}'.format(config['paths']['params'], 'PReFerSim/{demo_model}/param_{ns}.txt'), demo_model = demo_models, ns = cache_spectrum_nss)
+#         expand('{}/{}'.format(config['paths']['params'], 'PReFerSim/{demo_model}/demog.txt'), demo_model = demo_models),
+        expand('{}/{}'.format(config['paths']['data'], 'PReFerSim/bottleneck/bottleneck_n{ns}_rep{rep}.sfs_out.txt'), rep = reps, ns = cache_spectrum_nss)
 rule simulate_BGS_validation:
     '''
     simulate simplest scenarios for BGS strength validation. 
@@ -90,7 +90,31 @@ rule simulate_bottleneck:
     resources: mem_mb=200, time_min=180
     conda: 'envs/slim.yaml'
     shell:
-        """slim -d u=1e-7 -d rep={wildcards.rep} -d N={wildcards.N_bottleneck} -d "mode='3classes'" -m {params.sim_script}"""
+        """slim -d u=1e-7 -d rep={wildcards.rep} -d N={N_bottleneck} -d "mode='3classes'" -m {params.sim_script}"""
+
+
+rule create_PReFerSim_demogs:
+    '''
+    create demog files for PReFerSim
+    '''
+    output:
+        demog = '{}/{}'.format(config['paths']['params'], 'PReFerSim/{demo_model}/demog.txt')
+    resources: mem_mb = 200
+    script:
+        '{}/{}'.format(config['paths']['scripts'], 'create_prefersim_param.py')
+
+rule create_PReFerSim_params:
+    '''
+    create parameter files for PReFerSim
+    '''
+    params:
+        demog = '{}/{}'.format(config['paths']['params'], 'PReFerSim/{demo_model}/demog.txt'),
+        out_dir = '{}/{}'.format(config['paths']['data'], 'PReFerSim/{demo_model}')
+    output:
+        param = '{}/{}'.format(config['paths']['params'], 'PReFerSim/{demo_model}/param_{ns}.txt')
+    resources: mem_mb = 200
+    script:
+        '{}/{}'.format(config['paths']['scripts'], 'create_prefersim_param.py')
 
         
 rule simulate_prf_bottleneck:
@@ -98,27 +122,14 @@ rule simulate_prf_bottleneck:
     simulate from PReFerSim
     '''
     params:
-        par = '{}/{}'.format(config['paths']['params'], 'PReFerSim/bottleneck/param.txt')
-        demog = '{}/{}'.format(config['paths']['params'], 'PReFerSim/bottleneck/demog.txt')
+        par = '{}/{}'.format(config['paths']['params'], 'PReFerSim/bottleneck/param_{ns}.txt')
     output:
-        full = '{}/{}'.format(config['paths']['data'], 'PReFerSim/bottleneck/Output.{rep}.full_out.txt')
-        popsfs = '{}/{}'.format(config['paths']['data'], 'PReFerSim/bottleneck/Output.{rep}.popsfs_out.txt')
-        sfs = '{}/{}'.format(config['paths']['data'], 'PReFerSim/bottleneck/Output.{rep}.sfs_out.txt')
-    resources: mem_mb=200, time_min=180
-    conda: 'envs/slim.yaml'
+        full = '{}/{}'.format(config['paths']['data'], 'PReFerSim/bottleneck/bottleneck_n{ns}_rep{rep}.full_out.txt'),
+        popsfs = '{}/{}'.format(config['paths']['data'], 'PReFerSim/bottleneck/bottleneck_n{ns}_rep{rep}.popsfs_out.txt'),
+        sfs = '{}/{}'.format(config['paths']['data'], 'PReFerSim/bottleneck/bottleneck_n{ns}_rep{rep}.sfs_out.txt')
+    resources: mem_mb=300, time_min=60
     shell:
-        """GSL_RNG_SEED={wildcards.rep} GSL_RNG_TYPE=mrg utils/PReFerSim {params.par} {wildcards.rep}"""
-        
-rule create_PReFerSim_params:
-    '''
-    create parameter files for PReFerSim
-    '''
-    output:
-        demog = '{}/{}'.format(config['paths']['params'], 'PReFerSim/{demo_model}/demog.txt')
-        param = '{}/{}'.format(config['paths']['params'], 'PReFerSim/{demo_model}/param.txt')
-    resources: mem_mb = 50
-    script:
-        '{}/{}'.format(config['paths']['scripts'], 'create_prefersim_param.py')
+        """module load gsl; GSL_RNG_SEED={wildcards.rep} GSL_RNG_TYPE=mrg utils/PReFerSim {params.par} {wildcards.rep}"""
 
 
 rule simulate_prf_constant_size:
@@ -126,13 +137,13 @@ rule simulate_prf_constant_size:
     simulate from PReFerSim
     '''
     params:
-        par = '{}/{}'.format(config['paths']['params'], 'PReFerSim/constant/param.txt')
+        par = '{}/{}'.format(config['paths']['params'], 'PReFerSim/constant/param.txt'),
         demog = '{}/{}'.format(config['paths']['params'], 'PReFerSim/constant/demog.txt')
     output:
-        full = '{}/{}'.format(config['paths']['data'], 'PReFerSim/constant/Output.{rep}.full_out.txt')
-        popsfs = '{}/{}'.format(config['paths']['data'], 'PReFerSim/constant/Output.{rep}.popsfs_out.txt')
+        full = '{}/{}'.format(config['paths']['data'], 'PReFerSim/constant/Output.{rep}.full_out.txt'),
+        popsfs = '{}/{}'.format(config['paths']['data'], 'PReFerSim/constant/Output.{rep}.popsfs_out.txt'),
         sfs = '{}/{}'.format(config['paths']['data'], 'PReFerSim/constant/Output.{rep}.sfs_out.txt')
-    resources: mem_mb=200, time_min=180
+    resources: mem_mb=200, time_min=60
     conda: 'envs/slim.yaml'
     shell:
         """GSL_RNG_SEED={wildcards.rep} GSL_RNG_TYPE=mrg utils/PReFerSim {params.par} {wildcards.rep}"""
